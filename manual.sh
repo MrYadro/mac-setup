0c07ccde084f3c598d7898064b40bef88f64cff4
Y.Yadrushnikov <me@yay.qa>
2021-10-30 23:20:43 +0300

    first commit

---
+++ manual.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zdharma/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
