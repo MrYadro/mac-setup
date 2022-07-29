d575126034abde586e8b62f2d0484ca66440d25b
Y.Yadrushnikov <me@yay.qa>
2022-07-29 20:22:56 +0300

    update deps

--- manual.sh
+++ manual.sh
@@ -1,2 +1,2 @@
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone https://github.com/z-shell/F-Sy-H.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/F-Sy-H
