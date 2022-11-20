05cd54f52ffce9a6180a7b1e914554edf355dc1e
Y.Yadrushnikov <me@yay.qa>
2022-11-21 00:54:22 +0300

    move to brewfile

--- manual.sh
+++
@@ -1,2 +0,0 @@
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/z-shell/F-Sy-H.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/F-Sy-H
